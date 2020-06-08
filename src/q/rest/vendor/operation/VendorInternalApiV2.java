597c4c673c67410454e84574297c013ccb7bf0cb
fareed <fareed@qetaa.com>
2020-06-08 20:06:03 +0300

    edited max search to open

--- src/q/rest/vendor/operation/VendorInternalApiV2.java
+++ src/q/rest/vendor/operation/VendorInternalApiV2.java
@@ -1419,7 +1419,7 @@ public class VendorInternalApiV2 {
             Helper h = new Helper();
             String dateString = h.getDateFormat(new Date(), "yyyy-MM-dd");
             String sql = "select z.* from (select vendor_id, count(*) from vnd_search_keyword where created > '"+dateString+"' group by vendor_id order by count desc) z";
-            List<Object> ss = dao.getNativeMax(sql, 10);
+            List<Object> ss = dao.getNative(sql);
             List<VendorSearchCount> vscs = new ArrayList<>();
             for(Object o : ss){
                 if(o instanceof Object[]){
